0c9c37a6ccc6db6e9c6bff78d431ff636999f4d2
ckovach <ckovach@4e33aaf1-3bdf-43e8-baf7-e7e736085cc8>
2015-08-09 02:15:53 +0000

    Change pre-to-post linear warping interpolation to spline. Trilinear is often too blurry.
    git-svn-id: https://saccade.neurosurgery.uiowa.edu/svn/KovachToolbox@634 4e33aaf1-3bdf-43e8-baf7-e7e736085cc8

--- run_fsl_script.m
+++ run_fsl_script.m
@@ -35,7 +35,7 @@ end
 com1 = sprintf('cp %s %sT1temp_orig%s',fullfile(pthpreop,fnpreop),sid,ext);
 com2 = sprintf('fslreorient2std %sT1temp_orig  %sT1temp',sid,sid);
 com3 = 'echo ''Linear coregistration of preop and postop images...''';
-com4 = sprintf('flirt -in %s -ref %sT1temp -o %spostop_aligned -omat %spost_to_pre.mat  -bins 256 -cost corratio -searchrx -180 180 -searchry -180 180 -searchrz -180 180 -dof 12  -interp trilinear &',fullfile(pthpostop,fnpostop),sid,sid,sid);
+com4 = sprintf('flirt -in %s -ref %sT1temp -o %spostop_aligned -omat %spost_to_pre.mat  -bins 256 -cost corratio -searchrx -180 180 -searchry -180 180 -searchrz -180 180 -dof 12  -interp spline &',fullfile(pthpostop,fnpostop),sid,sid,sid);
 com5 = sprintf('fsl_anat --weakbias  -i %sT1temp -o %s --noreorient --clobber',sid,odir);
 com6 =  sprintf('fslmaths %spostop_aligned  %s.anat%spostop_aligned',sid,odir,filesep);
 com7 =  sprintf('mv %spost_to_pre.mat  %s.anat%spost_to_pre.mat',sid,odir,filesep);
